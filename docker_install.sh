e6ca9ff24db1e84e9fc33ee51808f24d33adcb60
vkushnirenko-luxoft <vkushnirenko@luxoft.com>
2018-03-09 19:53:51 +0200

    fix

--- docker_install.sh
+++ docker_install.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
-#Script installs docker
 
+#install docker
 curl -fsSL get.docker.com -o get-docker.sh
 sudo sh get-docker.sh
+sudo groupadd docker && sudo usermod -aG docker $USER
+
+#Making changes for registry
+wget  https://raw.githubusercontent.com/vkushnirenko-luxoft/scripts/master/modify_docker_for_registry.sh
+chmod +x modify_docker_for_registry.sh
+./modify_docker_for_registry.sh
