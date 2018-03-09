311c27f81a75e5a6a416dd535eab0045601445b4
vkushnirenko-luxoft <vkushnirenko@luxoft.com>
2018-03-09 20:15:47 +0200

    Fix Modify... with ubuntu versions

--- modify_docker_for_registry.sh
+++ modify_docker_for_registry.sh
@@ -1,6 +1,19 @@
 #!/bin/bash
-#Script changes docker environment to be able work with docker registry
+#Script changes docker environment to be able working with docker registry
 
+#check an os version
+version=$(lsb_release -sr)
+case $version in
+14.*)
+    sudo echo 'DOCKER_OPTS="--insecure-registry sdl.docker:5000"' | sudo tee -a /etc/default/docker
+    ;;
+*)
+    sudo sed -i -e 's/^ExecStart.*/ExecStart=\/usr\/bin\/dockerd -H fd:\/\/ --insecure-registry sdl.docker:5000/g' /lib/systemd/system/docker.service
+    sudo systemctl daemon-reload
+esac
+
+#define registry ip address
 sudo echo '172.30.23.4 sdl.docker' | sudo tee -a /etc/hosts
-sudo echo 'DOCKER_OPTS="--insecure-registry sdl.docker:5000"' | sudo tee -a /etc/default/docker
+
+#restart docker service to apply changes
 sudo service docker restart
